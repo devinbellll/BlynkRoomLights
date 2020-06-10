121950ec08b96cd9f3b9d3bbeaff966a6e146a2f
dbell44@uwo.ca <dbell44@uwo.ca>
2020-06-09 23:24:08 -0400

    got serial interrupt working

--- serialdev/serialdev.ino
+++ serialdev/serialdev.ino
@@ -1,21 +1,64 @@
-String mode = "";
-String manualData = "";
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
+#define USART_BAUDRATE 9600
+#define MYUBRR (((F_CPU / (USART_BAUDRATE * 16UL))) - 1)
+
+void setup() {
+
+  pinMode(13, OUTPUT);
+  digitalWrite(13, LOW);
+  UCSR0B = (1 << RXEN0) | (1 << TXEN0);   // Turn on the transmission and reception circuitry
+  UCSR0C = (1 << UCSZ00) | (1 << UCSZ01); // Use 8-bit character sizes
+
+  UBRR0H = (MYUBRR >> 8); // Load upper 8-bits of the baud rate value into the high byte of the UBRR register
+  UBRR0L = MYUBRR; // Load lower 8-bits of the baud rate value into the low byte of the UBRR register
 
 
-void setup() 
+
+  UCSR0B |= (1 << RXCIE0); // Enable the USART Recieve Complete interrupt (USART_RXC)
+
+  sei(); // Enable the Global Interrupt Enable flag so that interrupts can be processed
+
+}
+
+
+String rxstring = "";
+bool RECEIVEDLINE = false;
+
+//intrerupt on receive
+ISR (USART_RX_vect)
 {
-  Serial.begin(9600);
+  char ReceivedByte;
+  ReceivedByte = UDR0; // Fetch the received byte value into the variable "ByteReceived"
+
+  if (ReceivedByte == '\n')
+  {
+    RECEIVEDLINE = true;
+  }
+  else 
+  {
+    rxstring += ReceivedByte;
+  }
+  UDR0 = ReceivedByte; // Echo back the received byte back to the computer
+  //digitalWrite(13, HIGH);
+
+}
+
+void loop() {
+  if(RECEIVEDLINE) 
+  {
+    RECEIVEDLINE = false;
+    digitalWrite(13, HIGH);
+
+    if(rxstring == "test")
+    {
+      digitalWrite(13, LOW);
+    }
+    rxstring = "";
+  }
+
   
-  UCSR0B |= (1 << RXCIE0);
-  sei();
-}
-
-void loop() 
-{
-}
-
-
-void ISRwhatever(void)
-{
-
+  
 }
